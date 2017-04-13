0d8624c74f2b61b9b8678d8e34040409c81f3022
unknown <jehung@me.com>
2017-04-12 23:08:48 -0700

    Modify .r file

--- loan_analysis.r
+++ loan_analysis.r
@@ -25,7 +25,7 @@ index = sample(nrow(loan), 1000)
 cols = c('CreditGrade', 'Term', 'LoanStatus', 'BorrowerRate', 'LenderYield', 'EstimatedLoss', 'EstimatedReturn', 
          'ProsperRatingNumeric', 'ProsperRatingAlpha', 'ProsperScore', 'ListingCategory..numeric.',
          'EmploymentStatus', 'EmploymentStatusDuration', 'IsBorrowerHomeowner', 'ProsperPrincipalBorrowed',
-         'LoanOriginalAmount', 'Recommendations', 'Investors')
+         'LoanOriginalAmount', 'Recommendations', 'Investors', 'DebtToIncomeRatio', 'StatedMonthlyIncome', 'MonthlyLoanPayment')
 loan_subset = loan[index, cols]
 
 ## Explore correlation between CreidtGrade and and ProsperRatingA
@@ -60,21 +60,33 @@ term = ggplot(loan, aes(x=Term)) + geom_bar() +
 term
 
 ## Explore relationship between PropserRatingAlpha, LenderYield, and Term
+## Major finding
 rel = ggplot(data=loan_subset, aes(x=ProsperRatingAlpha, y=LenderYield)) + geom_boxplot() +
     facet_wrap(~Term)
 rel
     
 ## Explore relationship between # Recommendations and # Investors, and Term
+## Another major finding- more recommendations does not lead to more investors
+## why? Is more recommendation a sign of bad credit?
 rec = ggplot(data=loan, aes(x=Recommendations, y=Investors)) + geom_point() +
-    facet_wrap(~Term) + 
-    scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0,5))
+    facet_wrap(~Term) + scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0,5))
 rec
 
 ## Noticed that LendersYield are lower for high recommendations. Why?
-rec1 = ggplot(data=loan, aes(x=Recommendations, y=LenderYield)) + geom_point() +
+## Is there a relationship between Recommendations and LendersYield?
+## skipped
+rec1 = ggplot(data=loan, aes(x=Recommendations, y=LenderYield)) + geom_point(alpha=0.1) +
     facet_wrap(~Term) 
     scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0,5))
 rec1
+
+## One way to figure the above question is to see if Recommendations are correlated with CreditRatings
+## Now here
+recRating = ggplot(data=loan_subset, aes(x=ProsperRatingAlpha, y=Recommendations)) + geom_boxplot() +
+    facet_wrap(~Term)
+#+ facet_wrap(~Term) + scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0,5))
+recRating
+
 
 ## Explore if high recoomendations are associated with bad credit
 ## By investigating if higher BorrowerRate is related to lower ProsperRatingAlpha
@@ -83,38 +95,81 @@ rate_rating
 
 ## Is the above confounded by length of loan term?
 ## No, the above pattern holds for all temrs
+### Major finding
 rate_groups = group_by(loan, ProsperRatingAlpha)
 rate_rec = summarise(rate_groups, 
-                      rec_mean = mean(Recommendations),
-                      rec_median = median(Recommendations),
-                      n =n())
+                      recommendation_mean = mean(Recommendations),
+                      recommendation_median = median(Recommendations),
+                      recommendation_sd = sd(Recommendations))
 rate_rec = arrange(rate_rec, ProsperRatingAlpha)
 
-insight1 = ggplot(rate_rec, aes(ProsperRatingAlpha, y=rec_mean, group=1)) + geom_line() +
-    ylab('Number of Recommendations')
+melted = melt(rate_rec, id='ProsperRatingAlpha')
+
+insight1 = ggplot(melted, aes(ProsperRatingAlpha, y=value, color=variable, group=variable)) + geom_line() +
+    geom_point() + ylab('Recommendations of Loan')
 insight1
 
-
 ## A summary table by # of Recommendation, mean yield, and term
+## Major finding 
 rec_groups = group_by(loan, Recommendations)
 rec_rate = summarise(rec_groups,
                      mean_yield = mean(LenderYield),
                      median_yield = median(LenderYield),
                      term12 = sum(Term == 12),
                      term36 = sum(Term == 36),
-                     term60 = sum(Term == 60),
-                     n =n())
+                     term60 = sum(Term == 60))
 rec_rate = arrange(rec_rate, Recommendations)
 
+rec_rate
 
-insight2 = ggplot(rec_rate, aes(Recommendations, y=term12, group=1)) + geom_line() + 
-    scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0, 5))
-insight3 = ggplot(rec_rate, aes(Recommendations, y=term36, group=1)) + geom_line() +
-    scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0, 5))
-insight4 = ggplot(rec_rate, aes(Recommendations, y=term60, group=1)) + geom_line() +
-    scale_x_continuous(breaks=seq(0, 5, 1), limit=c(0, 5))
-
-grid.arrange(insight2, insight3, insight4, ncol=3)
+insight2 = ggplot(rec_rate, aes(factor(Recommendations), y=mean_yield, group=1)) + geom_point() + geom_line() +
+    xlab('Number of Recommendations') + ylab('Mean LenderYield')
+insight2
 
 
 ## What happens when the credit rating is NA?
+
+
+
+## Investigate the relationship between LendersYield and DebtIncome ratio
+## Find correlation r - low
+with(loan, cor.test(DebtToIncomeRatio, LenderYield))
+
+debt_income_sub = ggplot(subset(loan_subset, !is.na(DebtToIncomeRatio)), aes(DebtToIncomeRatio, LenderYield)) + 
+    geom_point(alpha=0.05)
+debt_income_sub
+
+
+debt_income = ggplot(subset(loan, !is.na(DebtToIncomeRatio)), aes(DebtToIncomeRatio, LenderYield)) + 
+    geom_point(alpha=0.05)
+debt_income
+
+
+## use this graph
+debt_income_log = ggplot(subset(loan_subset, !is.na(DebtToIncomeRatio)), aes(DebtToIncomeRatio, LenderYield)) + 
+    geom_point(alpha=0.05) + scale_x_log10() + scale_y_log10() +
+    facet_wrap(~ProsperRatingNumeric)
+debt_income_log
+
+
+## Investigate the relationship between LendersYield and Payment/Income ratio
+## Find correlation r
+loan['PaymentIncomeRatio'] = loan['MonthlyLoanPayment'] / loan['StatedMonthlyIncome']
+loan['PaymentIncomeRatio'][loan['PaymentIncomeRatio']==Inf] = NA
+
+
+with(subset(loan, !is.na(PaymentIncomeRatio)), cor.test(PaymentIncomeRatio, LenderYield))
+
+pmt_income = ggplot(subset(loan, !is.na(PaymentIncomeRatio)), aes(PaymentIncomeRatio, LenderYield)) + 
+    geom_point(alpha=0.05) + facet_wrap(~ProsperRatingNumeric) + scale_x_continuous(limit=c(0,1)) +
+    scale_x_log10() + scale_y_log10()
+    
+pmt_income
+
+
+## Conclude whether DebtIncome ratio is highly correlated with PaymentIncoem ratio
+## Find correlation r
+with(loan, cor.test(DebtToIncomeRatio, PaymentIncomeRatio))
+
+
+
